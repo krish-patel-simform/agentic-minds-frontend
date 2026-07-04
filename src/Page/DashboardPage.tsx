6c89a7732393e2346d4c7692ef191b46f87f14ee
krish-patel-simform <krish.patel@simformsolutions.com>
2026-07-04 15:39:20 +0530

    resolved scrolling issue

--- src/Page/DashboardPage.tsx
+++ src/Page/DashboardPage.tsx
@@ -4,15 +4,13 @@ import ErrorBoundary from "../ErrorBoundary/ErrorBoundary";
 
 export default function DashboardPage() {
   return (
-    <div className="flex flex-col h-screen overflow-hidden bg-[#F8F9FD]">
-      <div className="flex flex-1 overflow-hidden">
-        <Sidebar />
-        <main className="flex-1 overflow-y-auto p-8">
-          <ErrorBoundary>
-            <Outlet />
-          </ErrorBoundary>
-        </main>
-      </div>
+    <div className="flex flex-1 overflow-hidden">
+      <Sidebar />
+      <main className="flex-1 overflow-y-auto p-8">
+        <ErrorBoundary>
+          <Outlet />
+        </ErrorBoundary>
+      </main>
     </div>
   );
 }
